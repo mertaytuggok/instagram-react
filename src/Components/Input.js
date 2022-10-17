c39047057e0986cdd150062abf96cb9bcfb4bc07
mertaytuggok <mertaytuggok@gmail.com>
2022-10-17 18:42:11 +0300

    Login Page Design

---
+++ src/Components/Input.js
@@ -0,0 +1,41 @@
+import { type } from "@testing-library/user-event/dist/type";
+import React, { useEffect, useRef, useState } from "react";
+
+export default function Input({ label, type = "text", ...props }) {
+  const inputRef = useRef();
+  const [show, setShow] = useState(false);
+  const [inputType, setType] = useState(type);
+
+  useEffect(() => {
+    if (show) {
+      setType("text");
+      inputRef.current.focus();
+    } else if (type === "password") {
+      setType("password");
+      inputRef.current.focus();
+    }
+  }, [show]);
+  return (
+    <label className=" block relative flex  bg-zinc-50 border rounded-sm focus-within:border-gray-400">
+      <input
+        ref={inputRef}
+        type={inputType}
+        required={true}
+        className=" px-2  outline-none  w-full h-[38px] text-xs peer valid:pt-[10px]"
+        {...props}
+      />
+      <small className="  absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5 ">
+        {label}
+      </small>
+      {type === "password" && props?.value && (
+        <button
+          type="button"
+          onClick={() => setShow((show) => !show)}
+          className="   h-full flex items-center text-sm pr-2 font-semibold"
+        >
+          {show ? "Hide" : "Show"}
+        </button>
+      )}
+    </label>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:  {
+        brand: "#0095f6",
+        facebook: "#385185",
+        link: "#00376b"
+      },
+      backgroundImage: {
+        'logo-pattern': 'url(https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png)'
+      }
+    },
   },
   plugins: [],
 }
